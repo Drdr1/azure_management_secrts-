836aa1d2da8e9f5bbd2ef7ad497169f7f054c9e7
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-03-23 21:10:13 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,32 +1,8 @@
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-}
-
-variable "client_id" {
-  description = "Azure service principal client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Azure service principal client secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  sensitive   = true
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
 }
