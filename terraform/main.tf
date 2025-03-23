05dc0a40d8042d66d3b21d2aacf709395e3672d9
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-03-23 21:53:58 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
+# main.tf
+
+# Generate a random suffix
+resource "random_id" "suffix" {
+  byte_length = 4  # Generates a 4-byte (8-character) random suffix
+}
+
+# Define the Resource Group with a unique name
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "secret-management-rg-${random_id.suffix.hex}"  # Append a random suffix
   location = var.location
 }
