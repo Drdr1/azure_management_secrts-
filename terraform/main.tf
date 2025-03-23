ae4b53e080d0dc4767d00d69497cd1edd6ac48f2
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-03-23 20:40:42 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  features {}
-
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
