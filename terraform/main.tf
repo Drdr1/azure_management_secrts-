b36b85c89cd019cd0f03f711530aee43c8d7d00f
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-03-23 21:51:03 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,4 @@
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "secret-management-rg-${random_id.suffix.hex}"  # Append a random suffix
-}
-
-variable "location" {
-  description = "Azure region for resources"
-  type        = string
-  default     = "eastus"
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4  # Generates a 4-byte (8-character) random suffix
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
