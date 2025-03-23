dd2aa30fea8d7165073a8f8b9d2f03da723cdccf
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-03-23 22:14:28 +0200

    Update automation-runbooks.tf

--- terraform/automation-runbooks.tf
+++ terraform/automation-runbooks.tf
@@ -1,10 +1,9 @@
 resource "azurerm_automation_account" "automation_account" {
-  name                = "secret-automation-account"
+  name                = "secret-automation-account-${random_id.suffix.hex}"  # Append a random suffix
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Basic"
 }
-
 resource "azurerm_automation_runbook" "runbook" {
   name                    = "check-expiring-secrets"
   location                = azurerm_resource_group.rg.location
